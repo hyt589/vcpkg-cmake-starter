f57c7e4e4d1821a5621f62c36439bae3fb7fc93d
huangyutong01 <huangyutong01@MacBook-Pro.local>
2024-11-27 12:31:16 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Overview
+
+This is a C/C++ project template that uses `cmake` and `vcpkg` to manage build and dependencies. Fork this repo to bootstrap your project.
+
+# VCPKG
+
+There are 3 ways to use the `vcpkg` program with this project template:
+
+## 1.1 SDK
+
+`vcpkg` supports using a set of previously built and exported dependency libraries without actually installing the program itself. This is useful for sharing prebuilt dependencies during development to avoid building everything from source, which may become time consuming as the number of dependencies grow.
+
+To do this, you will first need to obtain an export of dependent libraries via steps described in [the official docs](https://learn.microsoft.com/en-us/vcpkg/commands/export). This will produce a package of the libraries in a format that you specified.
+
+Then, extract the generated export into the filesystem, and provide the extrated path to `cmake` via the variable `EXPORT_SDK_PATH`:
+
+```bash
+cmake -B build -D EXPORT_SDK_PATH=<extracted/path> --preset default
+```
+
+## 1.2 Install `vcpkg` on system
+
+If you have installed `vcpkg` on your system and properly configured the `VCPKG_ROOT` environment variable, as described in [the installation guide](https://learn.microsoft.com/en-us/vcpkg/get_started/get-started?pivots=shell-bash), this project can then use the system `vcpkg` in [manifest mode](https://learn.microsoft.com/en-us/vcpkg/concepts/manifest-mode).
+
+The `vcpkg.json` and `vcpkg-configuration.json` is automatically generated when you invoke `cmake` for the first time:
+
+```bash
+cmake -B build --preset default
+```
+
+Refer to [official docs](https://learn.microsoft.com/en-us/vcpkg/) on how to manage dependencies with `vcpkg`.
+
+## 1.3 Use `vcpkg` as build dependency
+
+If `vcpkg` is not found on the system as described above, this project will automatically fetch `vcpkg` from the [official repo](https://github.com/microsoft/vcpkg) as a build dependency via cmake's `FetchContent` mechanic. The rest is the same as above, except that the `vcpkg` binary will be available in the build directory once the project has been configured via cmake:
+
+```bash
+cmake -B build --preset default
+./build/vcpkg add fmt spdlog # add any package you want
+./build/vcpkg install
+cmake --build build -- -j $(nproc)
+```
---
+++ cmake/vcpkg.toolchain.cmake
@@ -0,0 +1,97 @@
+if(DEFINED EXPORT_SDK_PATH
+   AND EXISTS ${EXPORT_SDK_PATH}/scripts/buildsystems/vcpkg.cmake)
+  message(STATUS "using an exported vcpkg sdk from ${EXPORT_SDK_PATH}")
+  set(CMAKE_TOOLCHAIN_FILE ${EXPORT_SDK_PATH}/scripts/buildsystems/vcpkg.cmake)
+
+elseif(EXISTS $ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
+  message(STATUS "using vcpkg installed in $ENV{VCPKG_ROOT}")
+  set(CMAKE_TOOLCHAIN_FILE $ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
+  if(WIN32)
+    set(VCPKG_PATH $ENV{VCPKG_ROOT}/vcpkg.exe)
+  else()
+    set(VCPKG_PATH $ENV{VCPKG_ROOT}/vcpkg)
+  endif()
+
+elseif(NOT DEFINED _WRAPPER_VCPKG_INCLUDE)
+  set(_WRAPPER_VCPKG_INCLUDE "vcpkg included") # include guard
+  message(STATUS "vcpkg not found on system, installing as build dependency")
+  include(FetchContent)
+  set(FETCHCONTENT_QUIET FALSE)
+  FetchContent_Declare(
+    vcpkg
+    GIT_REPOSITORY "https://github.com/microsoft/vcpkg.git"
+    GIT_SHALLOW 1
+    GIT_PROGRESS TRUE)
+
+  message(
+    STATUS
+      "Fetching vcpkg as cmake submodule from https://github.com/microsoft/vcpkg.git"
+  )
+  FetchContent_MakeAvailable(vcpkg)
+  message(STATUS "Fetched vcpkg")
+
+  FetchContent_GetProperties("vcpkg" SOURCE_DIR vcpkg_SOURCE_DIR)
+
+  # bootstrap vcpkg
+  message(STATUS "Bootstrapping vcpkg")
+  if(WIN32)
+    if(NOT EXISTS "${vcpkg_SOURCE_DIR}/vcpkg.exe")
+      execute_process(COMMAND cmd /c "${vcpkg_SOURCE_DIR}/bootstrap-vcpkg.bat"
+                              COMMAND_ERROR_IS_FATAL ANY)
+    endif(NOT EXISTS "${vcpkg_SOURCE_DIR}/vcpkg.exe")
+    set(VCPKG_PATH
+        "${vcpkg_SOURCE_DIR}/vcpkg.exe"
+        CACHE STRING "vcpkg path" FORCE)
+  else()
+    if(NOT EXISTS "${vcpkg_SOURCE_DIR}/vcpkg")
+      execute_process(COMMAND sh "${vcpkg_SOURCE_DIR}/bootstrap-vcpkg.sh"
+                              COMMAND_ERROR_IS_FATAL ANY)
+    endif(NOT EXISTS "${vcpkg_SOURCE_DIR}/vcpkg")
+    set(VCPKG_PATH
+        "${vcpkg_SOURCE_DIR}/vcpkg"
+        CACHE STRING "vcpkg path" FORCE)
+  endif()
+  message(STATUS "Bootstrapped vcpkg")
+
+  # include toolchain file
+  message(STATUS "setting toolchain file to vcpkg.cmake")
+  set(CMAKE_TOOLCHAIN_FILE ${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake)
+
+endif()
+
+function(vcpkg_create_manifest)
+  if(VCPKG_MANIFEST_CREATED)
+    message(STATUS "Found existing vcpkg manifest")
+    return()
+  endif(VCPKG_MANIFEST_CREATED)
+
+  set(options)
+  set(oneValueArgs NAME VERSION DIR INSTALL_DIR)
+  set(multiValueArgs)
+  cmake_parse_arguments("MANIFEST" "${options}" "${oneValueArgs}"
+                        "${multiValueArgs}" ${ARGN})
+
+  if(EXISTS "${MANIFEST_DIR}/vcpkg.json")
+    message(STATUS "Manifest file already created")
+    set(VCPKG_MANIFEST_DIR
+        ${MANIFEST_DIR}
+        CACHE STRING "manifest dir" FORCE)
+    return()
+  endif(EXISTS "${MANIFEST_DIR}/vcpkg.json")
+
+  execute_process(
+    COMMAND "${VCPKG_PATH}" "new" "--name" "${MANIFEST_NAME}" "--version"
+            "${MANIFEST_VERSION}" "--x-install-root=${MANIFEST_INSTALL_DIR}"
+    WORKING_DIRECTORY "${MANIFEST_DIR}" COMMAND_ERROR_IS_FATAL ANY)
+  execute_process(
+    COMMAND "${VCPKG_PATH}" "x-update-baseline" "--add-initial-baseline"
+    WORKING_DIRECTORY "${MANIFEST_DIR}")
+  execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink ${VCPKG_PATH}
+                          ${CMAKE_BINARY_DIR}/vcpkg)
+  set(VCPKG_MANIFEST_CREATED
+      true
+      CACHE BOOL "whether vcpkg is found" FORCE)
+  set(VCPKG_MANIFEST_DIR
+      ${MANIFEST_DIR}
+      CACHE STRING "manifest dir" FORCE)
+endfunction(vcpkg_create_manifest)
